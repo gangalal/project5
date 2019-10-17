061a6c7cc0b56fd110a3f11d7d7885f6c3624260
D-Glover <55523571+D-Glover@users.noreply.github.com>
2019-10-17 16:33:47 -0500

    Add files via upload

--- project5.c
+++ project5.c
@@ -1,8 +1,8 @@
 /*
  ===============================================================================
  Name        : temprature.c
- Author      : $(author)
- Version     :
+ Author      : Gangal Lama Dong, Daniel Glover, and Jessica Horner
+ Version     : v 2.0
  Copyright   : $(copyright)
  Description : main definition
  ===============================================================================
@@ -15,6 +15,7 @@
 #include <cr_section_macros.h>
 
 #include <stdio.h>
+
 #define PCONP (*(volatile unsigned int *)(0x400FC0C4))
 #define PCLKSEL0 (*(volatile unsigned int *)(0x400FC1A8))
 #define T0TCR (*(volatile unsigned int *)(0x40004004))
@@ -22,21 +23,26 @@
 #define AD0CR (*(volatile unsigned int *)(0x40034000))
 #define AD0GDR (*(volatile unsigned int *)(0x40034004))
 #define PINSEL1 (*(volatile unsigned int *)(0x4002C004))
+// TODO #define PINMODE0 (*(volatile unsigned int *)(0x4002 C040))
 
+// Initialize the timer (use timer instead of PWM to set high time and decay length)
+// TODO complete initialization
 void timerInit() {
-	PCONP |= (1 << 1);
-	PCLKSEL0 &= ~(1 << 3) | (1 << 2); // resetting clock selection
-	PCLKSEL0 |= (0 << 3) | (0 << 2); //
-	//to do complete init....
+	PCONP |= (1 << 1);					// set the PCADC bit to power ADC
+	PCLKSEL0 &= ~(1 << 3) | (1 << 2);	// reset clock selection - D type Reg?
+	// PCLKSEL0 |= (0 << 3) | (0 << 2);
+	// PINSEL1 = ( << );					// TODO Select timer pins
+	// PINMODE0 = ( << );					// TODO Select pin mode with timer functions
 }
 
+// Initialize the analog to digital converter
 void ADCInit() {
-	PCONP |= (1 << 1);
-	AD0CR = (1 << 21);
-	AD0GDR;
-	AD0CR = 0;
-	AD0CR = (1 << 21);
-	PINSEL1 = (1 << 14); //need to make sure
+	PCONP |= (1 << 1);		// set the PCADC bit to power ADC
+	AD0CR = (1 << 21);		// enable PDN bit to turn on ADC bit
+	AD0GDR;					// read GDR and clear result
+	AD0CR = 0;				// disable PDN Nbit to turn off ADC bit
+	AD0CR = (1 << 21);		// enable PDN bit to turn on ADC bit
+	PINSEL1 = (1 << 14); 	// TODO double check this line of code
 }
 
 static inline void timerStart() {
@@ -50,8 +56,9 @@ static inline void timerStop() {
 static inline void timerReset() {
 	T0TCR |= (1 << 1);
 	while (T0TCR != 0) {
+		// wait until timer has changed
 	}
-	T0TCR &= ~(1 << 1);
+	T0TCR &= ~(1 << 1);	//
 }
 
 static inline int timerRead_us() {
@@ -66,12 +73,12 @@ void wait_us(int us) {
 }
 
 int ADCRead(int channel) {
-	AD0CR = (1 << channel) | (0 << 8) | (1 << 21);
-	AD0CR |= (1 << 24);
+	AD0CR = (1 << channel) | (0 << 8) | (1 << 21);	// (0 << 8) is the default
+	AD0CR |= (1 << 24);								// start conversion
 	while (((AD0GDR >> 31) & 1) == 0) {
-
+		// wait until the ADC has finished the conversion
 	}
-	return (AD0GDR >> 4) & (0xFFF);
+	return (AD0GDR >> 4) & (0xFFF);		// read result, return result
 }
 
 int main(void) {
